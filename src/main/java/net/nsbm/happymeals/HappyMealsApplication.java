ffc8efb00310c2a02433b4e4fc1aa65fb6e665e1
PiyumiNP_HO <PiyumiNP_HO@PLCHOIT197.ntplc.int>
2023-05-10 10:49:27 +0530

    Added main java file

---
+++ src/main/java/net/nsbm/happymeals/HappyMealsApplication.java
@@ -0,0 +1,13 @@
+package net.nsbm.happymeals;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HappyMealsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HappyMealsApplication.class, args);
+    }
+
+}
